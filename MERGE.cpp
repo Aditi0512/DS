929e9386914a707ab4ab7c836175d4cec708edd3
Aditi0512 <101205106+Aditi0512@users.noreply.github.com>
2023-01-17 19:38:40 +0530

    Create MERGE.cpp

---
+++ MERGE.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+int c=0;
+void merge(int a[], int beg, int mid, int end)
+{
+    int i, j, k;
+    int n1 = mid - beg + 1;
+    int n2 = end - mid;
+
+    int LeftArray[n1], RightArray[n2];
+    for (int i = 0; i < n1; i++)
+    LeftArray[i] = a[beg + i];
+    for (int j = 0; j < n2; j++)
+    RightArray[j] = a[mid + 1 + j];
+
+    i = 0,
+    j = 0;
+    k = beg;
+    while (i < n1 && j < n2)
+    {
+        if(LeftArray[i] <= RightArray[j])
+        {
+            a[k] = LeftArray[i];
+            i++;
+        }
+        else
+        {
+            a[k] = RightArray[j];
+            j++;
+        }
+        k++;
+        c++;
+    }
+    while (i<n1)
+    {
+        a[k] = LeftArray[i];
+        i++;
+        k++;
+    }
+
+    while (j<n2)
+    {
+        a[k] = RightArray[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int a[], int beg, int end)
+{
+    if (beg < end)
+    {
+        int mid = (beg + end) / 2;
+        mergeSort(a, beg, mid);
+        mergeSort(a, mid + 1, end);
+        merge(a, beg, mid, end);
+    }
+}
+int main()
+{int t;
+cin>>t;
+while(t--)
+{int n;
+cin>>n;
+int a[n];
+for(int i=0;i<n;i++)
+cin>>a[i];
+mergeSort(a,0,n-1);
+for(int i=0;i<n;i++)
+{
+cout<<a[i]<<" ";}
+cout<<endl<<"comparsions="<<c<<endl;
+}}
